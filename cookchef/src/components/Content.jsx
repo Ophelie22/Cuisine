bfe6e0605862376cc6afdbe6b5c84cb50a04a2cd
Ophelie.L <ophelie.lahaye@otipass.com>
2024-01-30 16:01:56 +0100

    mise en place de mixing pour le debeugage

--- cookchef/src/components/Content.jsx
+++ cookchef/src/components/Content.jsx
@@ -6,11 +6,12 @@ import { data } from '../data/recipes'
 //on va iterer sur un tableaau 
     function Content() {
 const recipes = data;
-
+//br ligne 14 à retirer plus tard c'est la bordure qui change suivant la taille de l'écran pour le debug
     return (
         <div className="flex-fill container p-20">
             <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-            <div className={`card p-20 ${styles.contentCard}`}>
+            
+            <div className={`card p-20 ${styles.contentCard} br`}>
             
                 <div className={styles.grid}>
             
@@ -20,7 +21,7 @@ const recipes = data;
                 )) }
             
                 </div>
-            </div>
+        </div>
         </div>
     );
 }
