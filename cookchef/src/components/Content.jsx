4f1e191732c7760bc44f7a3947670c52a46a6254
Ophelie.L <ophelie.lahaye@otipass.com>
2024-01-30 14:43:25 +0100

    creation des props et data plus reeele et systeme de like mis en place

--- cookchef/src/components/Content.jsx
+++ cookchef/src/components/Content.jsx
@@ -9,17 +9,18 @@ const recipes = data;
 
     return (
         <div className="flex-fill container p-20">
-        <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-        <div className={`card p-20 ${styles.contentCard}`}>
+            <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+            <div className={`card p-20 ${styles.contentCard}`}>
             
-            <div className={styles.grid}>
+                <div className={styles.grid}>
             
-            { recipes.map((r) => (
-            <Recipe title={ r.title } image={ r.image } />
-            )) }
+                { recipes.map((r) => (
+                    <Recipe key={r.id}  title={ r.title } image={ r.image } />
+
+                )) }
             
+                </div>
             </div>
-        </div>
         </div>
     );
 }
--- cookchef/src/components/Recipe.jsx
+++ cookchef/src/components/Recipe.jsx
@@ -1,19 +1,29 @@
 import styles from "./Recipe.module.scss";
-
+import { useState } from "react";
 
 
 //du coup  on va recuperer 2  choses le titre et l'image
+//mise en place d'un bind pour récuperer le coeur est voir si il est cliqué ou pas
+//si liked est set à true alors on va mettre une classe css qui va changer la couleur du coeur
 function Recipe({title, image}) {
+//par defaut l'objet $ne sera pas liké.
+const [liked, setLiked] = useState(false);
+
+function handleClick() {
+setLiked(!liked);
+}
     return (
-        <div className={styles.recipe}>
-        <div className={styles.imageContainer}>
-            <img src={image} alt="recipe" />
-        </div>
-        <div
-            className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
-        >
-            <h3>{title}</h3>
-        </div>
+        //on va recuperer le click sur notre élément
+        <div onClick={handleClick}   className={styles.recipe}>
+            <div className={styles.imageContainer}>
+                <img src={image} alt="recipe" />
+            </div>
+            <div
+                className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
+            >
+                <h3 className="mb-10">{title}</h3>
+                <i className={`fa-solid fa-heart ${liked ? 'text-primary' : ''}`}></i>
+            </div>
         </div>
     );
 }
