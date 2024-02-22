72e99034f51b4a7c79c1de54bc1c8129e291df62
Ophelie.L <ophelie.lahaye@otipass.com>
2024-02-22 16:33:42 +0100

    ajout d'une barre de recherche fonctionnelle

--- cookchef/src/components/Content.jsx
+++ cookchef/src/components/Content.jsx
@@ -1,3 +1,4 @@
+import { useState }  from 'react';
 import styles from './Content.module.scss';
 import Recipe from './Recipe';
 import { data } from '../data/recipes'
@@ -6,22 +7,40 @@ import { data } from '../data/recipes'
 //on va iterer sur un tableaau 
     function Content() {
 const recipes = data;
+const [filter, setFilter] = useState('');
+
+function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+    }
+
 //br ligne 14 à retirer plus tard c'est la bordure qui change suivant la taille de l'écran pour le debug
     return (
         <div className="flex-fill container p-20">
             <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
             
-            <div className={`card p-20 ${styles.contentCard} br`}>
+            <div className={`card d-flex flex-column p-20 ${styles.contentCard} `}>
+            <div
+                className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
+                >
+
+                    <i className='fa-solid fa-magnifying-glass mr-15'></i>
+                    <input 
+                    onInput={handleInput}
+                    className='flex-fill' 
+                    type="text" 
+                    placeholder="Rechercher une recette" />
+                </div>
             
                 <div className={styles.grid}>
             
-                { recipes.map((r) => (
+                { recipes
+                .filter ((r) => r.title.toLowerCase().startsWith(filter))
+                .map((r) => (
                     <Recipe key={r.id}  title={ r.title } image={ r.image } />
-
-                )) }
-            
+                ))}           
                 </div>
-        </div>
+            </div>
         </div>
     );
 }
--- cookchef/src/components/Header.jsx
+++ cookchef/src/components/Header.jsx
@@ -1,23 +1,39 @@
 import styles from './Header.module.scss';
 import logo from '../assets/images/logo.png';
+import { useState } from 'react';
+import HeaderMenu from './HeaderMenu';
+
+
+    //on va commencer par ne pas afficher le menu
+    
 
 function Header() {
-    return (
+        const [showMenu, setShowMenu] = useState(false);
 
-        <header className={`${styles.header} d-flex flex-row align-items-center`}>
-            <i  class="fa-solid fa-bars mr-15"> </i> 
+        return (
+          <header className={`${styles.header} d-flex flex-row align-items-center`}>
             <div className="flex-fill">
-                <img src={logo} alt="logo" />
+              <img src={logo} alt="logof" />
             </div>
-            <ul>
-                <button className="mr-5 btn btn-reverse-primary">
-                    <i className="fa-solid fa-basket-shopping mr-5"></i>
-                    <span>panier</span>
-                </button>
-                <button className="btn btn-primary">connexion</button>
+            <ul className={styles.headerList}>
+              <button className="mr-5 btn btn-reverse-primary">
+                <i className="fa-solid fa-heart mr-5"></i>
+                <span>Wishlist</span>
+              </button>
+              <button className="btn btn-primary">Connexion</button>
             </ul>
-        </header>
-    );
-}
+            <i
+              onClick={() => setShowMenu(true)}
+              className={`fa-solid fa-bars ${styles.headerXs}`}
+            ></i>
+            {showMenu && (
+              <>
+                <div onClick={() => setShowMenu(false)} className="calc"></div>
+                <HeaderMenu />
+              </>
+            )}
+          </header>
+        );
+      }
 
 export default Header;
